607249b81ea17482bc4af60ad86e5f8ae53f0ab9
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-06-08 16:14:04 +0530

    setting up for notifications

---
+++ app/src/main/java/com/nikhil/gcsapp/GcsFirebaseMessagingService.java
@@ -0,0 +1,7 @@
+package com.nikhil.gcsapp;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class GcsFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "GcsFirebaseMsgService";
+}
--- app/src/main/java/com/nikhil/gcsapp/PostDetailActivity.java
+++ app/src/main/java/com/nikhil/gcsapp/PostDetailActivity.java
@@ -1,11 +1,14 @@
 package com.nikhil.gcsapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -13,6 +16,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -112,6 +116,23 @@ public class PostDetailActivity extends BaseActivity implements View.OnClickList
         // Listen for comments
         mAdapter = new CommentAdapter(this, mCommentsReference);
         mCommentsRecycler.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.back_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int i = item.getItemId();
+        if (i == R.id.action_back) {
+            startActivity(new Intent(this, MainActivity.class));
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
