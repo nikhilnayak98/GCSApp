d057c7d9c142c1b54f7e61bc95cac75a6c90b043
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-06-08 16:16:35 +0530

    rollback

--- app/src/main/java/com/nikhil/gcsapp/GcsFirebaseMessagingService.java
+++
@@ -1,7 +0,0 @@
-package com.nikhil.gcsapp;
-
-import com.google.firebase.messaging.FirebaseMessagingService;
-
-public class GcsFirebaseMessagingService extends FirebaseMessagingService {
-    private static final String TAG = "GcsFirebaseMsgService";
-}
